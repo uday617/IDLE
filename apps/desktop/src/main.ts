6d806c325cd6fc5b8a1e0f15d952b12c2e2b5efd
uday617 <udaysinghgurjar528@gmail.com>
2026-08-19 05:12:34 +0530

    feat: add electron main process

---
+++ apps/desktop/src/main.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron';
+import { join } from 'node:path';
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    minWidth: 1100,
+    minHeight: 700,
+    webPreferences: {
+      preload: join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (process.env.ELECTRON_RENDERER_URL) {
+    void window.loadURL(process.env.ELECTRON_RENDERER_URL);
+  } else {
+    void window.loadFile(join(__dirname, '../renderer/index.html'));
+  }
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
